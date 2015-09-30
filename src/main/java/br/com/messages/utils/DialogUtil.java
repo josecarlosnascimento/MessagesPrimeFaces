2d4d9fb99d6eb53a16ab06584fe5422add746c12
j.nascimento <jcarlos.nascimento@outlook.com>
2015-09-30 20:59:50 -0300

    Renomeação do arquivo .java

---
+++ src/main/java/br/com/messages/utils/DialogUtil.java
@@ -0,0 +1,25 @@
+package br.com.mensagemPrimeFaces.utils;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import org.primefaces.context.RequestContext;
+
+@ManagedBean
+public class DialogUtil {
+ 
+    public void showMessageInfo() {
+        RequestContext.getCurrentInstance().showMessageInDialog(new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Mensagem Info."));
+    }
+    
+    public void showMessageError(){
+    	RequestContext.getCurrentInstance().showMessageInDialog(new FacesMessage(FacesMessage.SEVERITY_ERROR,"Error","Mensagem Erro"));
+    }
+    
+    public void showMessageAlerta(){
+    	RequestContext.getCurrentInstance().showMessageInDialog(new FacesMessage(FacesMessage.SEVERITY_WARN,"Alert","Mensagem Alerta"));
+    }
+    
+    public void showMessageErrorFatal(){
+    	RequestContext.getCurrentInstance().showMessageInDialog(new FacesMessage(FacesMessage.SEVERITY_FATAL,"Fatal","Mensagem Erro Fatal"));
+    }
+}
